1ee97fe3d38b9f1c871fec87a4914030d7a6f331
0ddaniels <42749711+0ddaniels@users.noreply.github.com>
2019-03-07 14:00:13 -0600

    Add files via upload

---
+++ toggleFunctions.js
@@ -0,0 +1,41 @@
+function green25(){
+    const doc = document;
+    let boxes = doc.querySelectorAll(".boxes > div");
+    for(let b=0; b<boxes.length; b++){
+        boxes[b].style.backgroundColor = "rgb(0,64,0)";      
+}
+}
+
+
+function green50(){
+
+
+}
+
+
+function green75(){
+
+
+}
+
+
+function green100(){
+
+
+}
+
+
+function smallToggle(){
+
+
+}
+
+
+function mediumToggle(){
+
+}
+
+
+function largeToggle(){
+
+}
