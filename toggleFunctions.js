3d15619df6b76363a51bb807abb4c3a63741fb17
0ddaniels <42749711+0ddaniels@users.noreply.github.com>
2019-03-08 14:00:38 -0600

    Add files via upload

--- toggleFunctions.js
+++ toggleFunctions.js
@@ -2,40 +2,73 @@ function green25(){
     const doc = document;
     let boxes = doc.querySelectorAll(".boxes > div");
     for(let b=0; b<boxes.length; b++){
-        boxes[b].style.backgroundColor = "rgb(0,64,0)";      
+        boxes[b].classList.remove("green50");
+        boxes[b].classList.remove("green75");
+        boxes[b].classList.remove("green100");
+        boxes[b].classList.add("green25");
 }
 }
-
 
 function green50(){
-
-
+    const doc = document;
+    let boxes = doc.querySelectorAll(".boxes > div");
+    for(let b=0; b<boxes.length; b++){
+        boxes[b].classList.remove("green25");
+        boxes[b].classList.remove("green75");
+        boxes[b].classList.remove("green100");
+         boxes[b].classList.add("green50");
+       
+}
 }
 
 
 function green75(){
-
-
+    const doc = document;
+    let boxes = doc.querySelectorAll(".boxes > div");
+    for(let b=0; b<boxes.length; b++){
+        boxes[b].classList.remove("green50");
+        boxes[b].classList.remove("green25");
+        boxes[b].classList.remove("green100");
+        boxes[b].classList.add("green75");      
+}
 }
 
 
 function green100(){
-
-
+    const doc = document;
+    let boxes = doc.querySelectorAll(".boxes > div");
+    for(let b=0; b<boxes.length; b++){
+        boxes[b].classList.add("green100");      
+}
 }
 
 
 function smallToggle(){
-
-
+    const doc = document;
+    let boxes = doc.querySelectorAll(".boxes > div");
+    for(let b=0; b<boxes.length; b++){
+        boxes[b].classList.remove("half_box");
+        boxes[b].classList.remove("full_box");
+        boxes[b].classList.add("quarter_box");      
+}
 }
 
 
 function mediumToggle(){
-
+    const doc = document;
+    let boxes = doc.querySelectorAll(".boxes > div");
+    for(let b=0; b<boxes.length; b++){
+        boxes[b].classList.remove("quarter_box");
+        boxes[b].classList.remove("full_box");
+        boxes[b].classList.add("half_box");      
+}
 }
 
 
 function largeToggle(){
-
+    const doc = document;
+    let boxes = doc.querySelectorAll(".boxes > div");
+    for(let b=0; b<boxes.length; b++){
+        boxes[b].classList.add("full_box");      
+}
 }
